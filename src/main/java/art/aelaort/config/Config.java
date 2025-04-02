5685621119985b2ce92570f90bff2b66f0c957d1
ort <orthodoxplus@ya.ru>
2025-04-03 01:30:55 +0300

    Config

--- src/main/java/art/aelaort/config/Config.java
+++ src/main/java/art/aelaort/config/Config.java
@@ -3,11 +3,13 @@ package art.aelaort.config;
 import art.aelaort.DefaultS3Params;
 import art.aelaort.S3Params;
 import art.aelaort.properties.S3Properties;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class Config {
@@ -22,6 +24,12 @@ public class Config {
 	@Bean
 	public YAMLMapper yamlMapper() {
 		return new YAMLMapper();
+	}
+
+	@Bean
+	@Primary
+	public JsonMapper jsonMapper() {
+		return new JsonMapper();
 	}
 
 	@Bean
