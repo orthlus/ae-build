6cd12a88034e22a6d79e605ba1b3345625032d50
ort <orthodoxplus@ya.ru>
2025-09-23 20:24:04 +0300

    BuildLaunchUtils fix

--- src/main/java/art/aelaort/Main.java
+++ src/main/java/art/aelaort/Main.java
@@ -1,5 +1,7 @@
 package art.aelaort;
 
+import art.aelaort.build.BuildLaunchType;
+import art.aelaort.build.BuildLaunchUtils;
 import art.aelaort.no_spring.BuildListEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,9 @@ import static art.aelaort.utils.Utils.slice;
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
-		if (args.length >= 1 && args[0].equals("build")) {
+		if (args.length >= 1
+			&& args[0].equals("build")
+			&& BuildLaunchUtils.build(slice(args, 1)) != BuildLaunchType.build) {
 			new BuildListEntry().run(slice(args, 1));
 		} else {
 			SpringApplication.run(Main.class, args);
